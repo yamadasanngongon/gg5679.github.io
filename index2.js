5e504bc9283280d4b322fda2fa77b6049616b79e
yamadasanngongon <142006592+yamadasanngongon@users.noreply.github.com>
2023-11-17 20:59:50 +0900

    index2.js を更新

--- index2.js
+++ index2.js
@@ -1,6 +1,3 @@
-var items = ['おみくじの結果:大吉', 'おみくじの結果:中吉', 'おみくじの結果:小吉', 'おみくじの結果:凶','おみくじの結果:大凶'];
-
-//最大値は配列の「要素数」にする
-var random = Math.floor( Math.random() * items.length );
-
-console.log( items[random] );
+body {
+	background-image: url(https://i.pinimg.com/originals/d4/bd/bb/d4bdbbe7861583381a84b795e43ba45c.gif);
+}
