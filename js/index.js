d67f9121d6467bc88f5955183230c9cde539102e
yamadasanngongon <142006592+yamadasanngongon@users.noreply.github.com>
2023-11-23 13:01:34 +0900

    index.js を更新

--- js/index.js
+++ js/index.js
@@ -1,16 +1,9 @@
-document.getElementById('audio').pause();
-$(function () {
-  var audioBtn = $('.audio_button'),
-  audioWrap = $('.audio_wrap'),
-  audio = document.getElementById('audio');
+window.addEventListener('DOMContentLoaded', function(){
 
-  audioBtn.on('click', function () {
-    if( audioWrap.hasClass('play') ) {
-      audio.pause();
-      audioWrap.removeClass('play');
-    } else {
-      audio.play();
-      audioWrap.addClass('play');
-    }
+  const audioElement = document.querySelector("audio");
+
+  audioElement.addEventListener('loadeddata', (e)=> {
+    audioElement.muted = true;
+    audioElement.autoplay = true;
   });
 });
