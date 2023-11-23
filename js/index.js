6a9bc236b39bfce068c60da2232dff91f974a457
yamadasanngongon <142006592+yamadasanngongon@users.noreply.github.com>
2023-11-23 11:12:43 +0900

    index.js を更新

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
-document.getElementById("view_time").innerHTML = getNow();
+//音声の再生
+document.getElementById('audio').play();
 
-function getNow() {
-	var now = new Date();
-	var year = now.getFullYear();
-	var mon = now.getMonth()+1; //１を足すこと
-	var day = now.getDate();
-	var hour = now.getHours();
-	var min = now.getMinutes();
-	var sec = now.getSeconds();
-
-	//出力用
-	var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒"; 
-	return s;
-}
+//音声の一時停止
+document.getElementById('audio').pause();
